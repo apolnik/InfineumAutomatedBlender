bc1424737340a9af1b8e1d93b918ca8162c4df75
andrew <apolnik@stevens.edu>
2018-02-01 15:13:46 -0500

    All bugs should be gone now...

--- Actuator/Stepper/Proximity/Proximity.cpp
+++ Actuator/Stepper/Proximity/Proximity.cpp
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "SysParam.h"
-//#include <prussdrv.h>
-//#include <pruss_intc_mapping.h>
+#include <prussdrv.h>
+#include <pruss_intc_mapping.h>
 //https://github.com/luigif/hcsr04/blob/master/hcsr04.c
 
 Proximity::Proximity(){
@@ -17,37 +17,37 @@ Proximity::Proximity(){
 }
 int Proximity::measureDistance(double* ret_val){
 	
-	// tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
-	// prussdrv_init();
-	// /* Open PRU Interrupt */
-	// if (prussdrv_open (PRU_EVTOUT_0)) {
-	// 	// Handle failure
-	// 	fprintf(stderr, ">> PRU open failed\n");
-	// 	return -1;
-	// }
+	tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+	prussdrv_init();
+	/* Open PRU Interrupt */
+	if (prussdrv_open (PRU_EVTOUT_0)) {
+		// Handle failure
+		fprintf(stderr, ">> PRU open failed\n");
+		return -1;
+	}
 
-	// /* Get the interrupt initialized */
-	// prussdrv_pruintc_init(&pruss_intc_initdata);
+	/* Get the interrupt initialized */
+	prussdrv_pruintc_init(&pruss_intc_initdata);
 
-	// /* Get pointers to PRU local memory */
-	// void *pruDataMem;
-	// prussdrv_map_prumem(PRUSS0_PRU0_DATARAM, &pruDataMem);
-	// unsigned int *pruData = (unsigned int *) pruDataMem;
+	/* Get pointers to PRU local memory */
+	void *pruDataMem;
+	prussdrv_map_prumem(PRUSS0_PRU0_DATARAM, &pruDataMem);
+	unsigned int *pruData = (unsigned int *) pruDataMem;
 
-	// /* Execute code on PRU */
-	// printf(">> Executing HCSR-04 code\n");
-	// prussdrv_exec_program(0, "hcsr04.bin");
-	// prussdrv_pru_wait_event (PRU_EVTOUT_0);
-	// prussdrv_pru_clear_event(PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
-	// prussdrv_pru_disable(0);
-	// prussdrv_exit();
-	// *ret_val = (double) pruData[0] / 148.4376;
-	// distance = *ret_val;
-	// for(int i =SAMPLE_SIZE-1; i>0;i--){
-	// 	data[i]=data[i-1];
+	/* Execute code on PRU */
+	printf(">> Executing HCSR-04 code\n");
+	prussdrv_exec_program(0, "hcsr04.bin");
+	prussdrv_pru_wait_event (PRU_EVTOUT_0);
+	prussdrv_pru_clear_event(PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
+	prussdrv_pru_disable(0);
+	prussdrv_exit();
+	*ret_val = (double) pruData[0] / 148.4376;
+	distance = *ret_val;
+	for(int i =SAMPLE_SIZE-1; i>0;i--){
+		data[i]=data[i-1];
 
-	// }
-	// data[0]=distance;
+	}
+	data[0]=distance;
 
 }
 double Proximity::avgData(){
