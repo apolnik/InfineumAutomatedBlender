0fa1702e47fd6656a9829d4c8d9f316b44791a64
ttzy <tt14719@gmail.com>
2018-02-17 13:52:46 -0500

    Update FluidTranCntrl.h

--- FluidTranCntrl/FluidTranCntrl.h
+++ FluidTranCntrl/FluidTranCntrl.h
@@ -4,24 +4,37 @@
 #include "PumpCntrl.h"
 #include "HeatCntrl.h"
 #include "tempMeasurement.h"
-#include "weightMeasurement.h" 
+#include "weightMeasurement.h"
+#include "TimerAPI.h"
 
 class FluidTranCntrl{
-	public:
-	
-	int fluidid;
-	ValveCntrl* valves;
-	PumpCntrl* pumps;
-	HeatCntrl* lineHeat;	
-	tempMeasurement* tempSensors;
-	weightMeasurement* scales;
+public:
+	int fluidid[5];
+	int beakers[4];
+	HeatCntrl lineHeat[]; // number of heatings on tubes TBD
+	tempMeasurement tempSensors[4];
+	ValveCntrl valves[10];
+	PumpCntrl pumps[6];
+  	weightMeasurement scales[3];
 
+	// Constructor
 	FluidTranCntrl();
-
-	double getTemp();
-	int setTemp();
+	// Temperature measurement
+	double getTemp(int sensorID);
+	// Transferring Process
 	int activateTransfer();
-	double getWeightTransferred();
+	// Cleaning Process
+	int cleaningTransfer();
+	// Set the temperature
+	int setTemp(int sensorID);
+	// Set the pump power
+	int setPump(int pumpID);
+	// Measure the weight
+	double getWeightTransferred(int scaleID);
+	// Terminate Transferring
+	int terminateTransfer();
+	// Open openValves
+	void openValves(void* ID);
 
-}; 
+};
 #endif /* FluidTranCntrl_H */
